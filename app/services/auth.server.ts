2648808235054a5d2c72bb16173a70aafe6cfaeb
Jonathan Sirrine <sir15004@byui.edu>
2024-11-14 17:05:08 -0700

    dynamic auth callback url

--- app/services/auth.server.ts
+++ app/services/auth.server.ts
@@ -18,7 +18,10 @@ export const googleStrategy = new GoogleStrategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID!,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    callbackURL: "http://localhost:5173/auth/callback",
+    callbackURL:
+      process.env.NODE_ENV === "production"
+        ? "https://become-you.netlify.app/auth/callback"
+        : "http://localhost:5173/auth/callback",
     scope: "profile email https://www.googleapis.com/auth/calendar",
     includeGrantedScopes: true,
   },
